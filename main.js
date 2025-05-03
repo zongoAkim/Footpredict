cd4b529ea8703fe3e20bc16cd815e5872b3c514f
zongoAkim <fitzgeraldzongo@gmail.com>
2025-05-03 07:33:48 +0000

    Create main.js
    import React from "https://esm.sh/react";
    import ReactDOM from "https://esm.sh/react-dom";
    import AdminDashboard from "./admin_dashboard.js";
    
    const root = document.getElementById("root");
    ReactDOM.render(React.createElement(AdminDashboard), root);

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import AdminDashboard from "./admin_dashboard.js";
+
+const root = document.getElementById("root");
+ReactDOM.render(React.createElement(AdminDashboard), root);
